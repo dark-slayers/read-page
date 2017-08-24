6b7783556715db8ecb5786a1abc961e69dba4ca0
dark-slayers <23534878@qq.com>
2017-08-24 16:58:47 +0800

    添加章节阅读页面的键盘翻页事件

--- src/jsx/Chapter.jsx
+++ src/jsx/Chapter.jsx
@@ -5,10 +5,41 @@ import RequestUrl from '../js/RequestUrl.js';
 
 class ChapterButton extends React.Component {
   handleClick = (index) => {
-    this.props.call(index);
+    localStorage.bookId = this.props.bookId;
+    localStorage.chapterIndex = index;
+    PageUtil.reload();
+  }
+  handleClickTitle = () => {
+    PageUtil.redirect("title.html");
   }
   handleClickButton = () => {
     PageUtil.redirect("update_chapter.html");
+  }
+  componentDidMount() {
+    let self=this;
+    window.addEventListener('keyup', function(e) {
+      switch (e.keyCode) {
+        case 37:
+          {
+            e.preventDefault();
+            self.handleClick(self.props.index - 1);
+            break;
+          }
+        case 39:
+          {
+            e.preventDefault();
+            self.handleClick(self.props.index + 1);
+            break;
+          }
+        default:
+          {
+            break;
+          }
+      }
+    });
+  }
+  componentWillUnmount(){
+    window.removeEventListener("keyup");
   }
   render() {
     let divStyle = {
@@ -28,7 +59,9 @@ class ChapterButton extends React.Component {
           <span style={spanStyle} onClick={this.handleClick.bind(this, this.props.index - 1)}>
             <a href="javascript:;">上一页</a>
           </span>
-          <span style={spanStyle}><a href="javascript:;">目录</a></span>
+          <span style={spanStyle} onClick={this.handleClickTitle}>
+            <a href="javascript:;">目录</a>
+          </span>
           <span style={spanStyle} onClick={this.handleClick.bind(this, this.props.index + 1)}>
             <a href="javascript:;">下一页</a>
           </span>
@@ -41,24 +74,15 @@ class ChapterButton extends React.Component {
   }
 }
 class Chapter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = this.props;
-  }
-  handleCall = (index) => {
-    localStorage.bookId=this.state.chapter.bookId;
-    localStorage.chapterIndex=index;
-    PageUtil.reload();
-  }
   render() {
-    var rawHTML={
-           __html:this.state.chapter.content.replace(/\n/g, "<br />")
-       };
+    var rawHTML = {
+      __html: this.props.chapter.content.replace(/\n/g, "<br />")
+    };
     return (
       <div>
-        <div>{this.state.chapter.title}</div>
+        <div>{this.props.chapter.title}</div>
         <div dangerouslySetInnerHTML={rawHTML}></div>
-        <div><ChapterButton index={this.state.chapter.index} call={this.handleCall}/></div>
+        <div><ChapterButton index={this.props.chapter.index} bookId={this.props.chapter.bookId}/></div>
       </div>
     );
   }
