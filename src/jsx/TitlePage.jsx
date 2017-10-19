3897d6f74b03b8bc6744289da2add8ac139126d1
dark-slayers <23534878@qq.com>
2017-10-19 16:31:04 +0800

    增加txt文件下载按钮

--- src/jsx/TitlePage.jsx
+++ src/jsx/TitlePage.jsx
@@ -1,48 +1,59 @@
 import React from 'react';
 import PageUtil from 'react-ui/lib/base/PageUtil.js';
 class TitleSpan extends React.Component {
-  handleClick=()=>{
-    console.log(this.props.title.index+" : "+this.props.title.title);
-    localStorage.chapterIndex=this.props.title.index;
+  handleClick = () => {
+    console.log(this.props.title.index + " : " + this.props.title.title);
+    localStorage.chapterIndex = this.props.title.index;
     PageUtil.redirect("chapter.html");
   }
   render() {
-    const spanStyle =
-    {
+    const spanStyle = {
       marginLeft: 10,
       marginRight: 10,
-      width:this.props.width,
-      textAlign: "center",
+      width: this.props.width,
+      textAlign: "center"
     };
-    return (<span style={spanStyle} onClick={this.handleClick}><a href="javascript:;">{this.props.title.title}</a></span>);
+    return (
+      <span style={spanStyle} onClick={this.handleClick}>
+        <a href="javascript:;">{this.props.title.title}</a>
+      </span>
+    );
   }
 }
 class TitleLine extends React.Component {
   render() {
-    const step=this.props.step;
-    const widthPercent = Math.round(10000 / step ) / 100.0+'%';
-    const divStyle={display: 'flex'};
+    const step = this.props.step;
+    const widthPercent = Math.round(10000 / step) / 100.0 + '%';
+    const divStyle = {
+      display: 'flex'
+    };
     return (
       <div style={divStyle}>
-        {
-          this.props.titleList.map(function(title)
-          {
-            return (<TitleSpan title={title} width={widthPercent} />);
-          })
-        }</div>
+        {this.props.titleList.map(function(title) {
+          return (<TitleSpan title={title} width={widthPercent}/>);
+        })
+}</div>
     );
   }
 }
 
 class TitlePage extends React.Component {
+  handleDownload = () => {
+    let bookId = localStorage.bookId;
+    let url=`/book/txt/${bookId}`;
+    window.open(url);
+  }
   render() {
-    let array=[];
+    let array = [];
     for (var i = 0; i < this.props.list.length; i++) {
-      array[i]={index:i,title:this.props.list[i]}
+      array[i] = {
+        index: i,
+        title: this.props.list[i]
+      }
     }
-    let step=3;
-    if(this.props.step){
-      step=this.props.step;
+    let step = 3;
+    if (this.props.step) {
+      step = this.props.step;
     }
     let list = [];
     for (let i = 0, len = array.length; i < len; i += step) {
@@ -50,13 +61,16 @@ class TitlePage extends React.Component {
     }
     return (
       <div>
-      {
-        list.map(function(titleList)
-        {
-          return (<TitleLine titleList={titleList} step={step}/>);
-        })
-      }
+        <div onClick={this.handleDownload}>
+          <a href="javascript:;">下载txt</a>
+        </div>
+        <div>
+          {list.map(function(titleList) {
+            return (<TitleLine titleList={titleList} step={step}/>);
+          })}
+        </div>
       </div>
+
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,55 @@
 var webpack = require("webpack");
 var path = require('path');
 var config = {
-    entry: {
-        vendor: ["jquery", "react", 'react-dom'],
-        "/target/chapter": "./src/entry/chapter.entry.js",
-        "/target/search_book": "./src/entry/search_book.entry.js",
-        "/target/title": "./src/entry/title.entry.js",
-        "/target/update_chapter": "./src/entry/update_chapter.entry.js",
-        "/target/add_book": "./src/entry/add_book.entry.js",
-        "/target/ech": "./src/entry/ech.entry.js",
-    },
-    output: {
-        path: path.resolve(__dirname, './'),
-        filename: "[name].bundle.js",
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: "/target/vendor.bundle.js"
-        })
-    ],
-    module: {
-        rules: [{
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['es2015', {
-                                modules: false
-                            }],
-                            ['stage-0'],
-                            ['react']
-                        ],
-                        plugins: ['syntax-dynamic-import']
-                    }
-                }]
-            },
-            {
-                test: /\.css$/,
-                use: [{
-                    loader: 'style-loader',
-                    options: {}
-                }, {
-                    loader: 'css-loader',
-                    options: {}
-                }]
-            }
-        ]
-    }
+  entry: {
+    vendor: ["jquery", "react", 'react-dom'],
+    "/target/chapter": "./src/entry/chapter.entry.js",
+    "/target/search_book": "./src/entry/search_book.entry.js",
+    "/target/title": "./src/entry/title.entry.js",
+    "/target/update_chapter": "./src/entry/update_chapter.entry.js",
+    "/target/add_book": "./src/entry/add_book.entry.js",
+    "/target/ech": "./src/entry/ech.entry.js",
+  },
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: "/target/vendor.bundle.js"
+    })
+  ],
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                modules: false,
+                "targets": {
+                  "browsers": ["Chrome >= 60"]
+                }
+              }],
+              ['stage-0'],
+              ['react']
+            ],
+            plugins: ['syntax-dynamic-import']
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }]
+      }
+    ]
+  }
 }
 module.exports = config;
